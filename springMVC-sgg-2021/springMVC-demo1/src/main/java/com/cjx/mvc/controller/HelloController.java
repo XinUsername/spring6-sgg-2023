d22a37786474820e9b98b4f2c6da306ca828ba6b
chenjiexin <chenjiexin@szzt.com.cn>
2024-02-18 16:01:36 +0800

    springmvc-demo1

---
+++ springMVC-sgg-2021/springMVC-demo1/src/main/java/com/cjx/mvc/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.cjx.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping( "/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "target";
+    }
+}
